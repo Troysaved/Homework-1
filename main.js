9f8f5ed532c35430361e26714ced7a5c5d380ee2
Troysaved <troysatchell@gmail.com>
2024-01-26 15:59:36 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// hiding the banner
+document.getElementById("hideButton").addEventListener("click", function() {
+    document.getElementById("banner").style.display = "none";
+});
+
+// surprise button
+document.getElementById("surpriseButton").addEventListener("click", function() {
+    alert("Surprise!");
+});
